ef61f76f47fcc435ac8b71c97d42804702af58c4
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-01-01 20:35:58 +0000

    fix(types): implement missing items in wlr_data_device

--- src/wlr/wlr_data_device.v
+++ src/wlr/wlr_data_device.v
@@ -11,7 +11,7 @@ module wlr
 #include "wlr/types/wlr_data_device.h"
 
 pub struct C.wlr_data_device_manager {
-	global       C.wl_global
+	global       &C.wl_global
 	data_sources C.wl_list
 
 	events struct {
@@ -31,17 +31,27 @@ enum Wlr_data_offer_type {
 }
 
 pub struct C.wlr_data_offer {
-	resource C.wl_resource
-	source   C.wlr_data_source
+	resource &C.wl_resource
+	source   &C.wlr_data_source
 	type     Wlr_data_offer_type
 
 	actions          u32
-	preferred_action C.wl_data_device_manager_dnd_action
+	preferred_action Wl_data_device_manager_dnd_action
 	in_ask           bool
 
 	wlr_private struct {
 		source_destroy C.wl_listener
 	}
+}
+
+pub struct C.wlr_data_source_impl {
+	send    fn (source &C.wlr_data_source, mime_type string, fd int)
+	accept  fn (source &C.wlr_data_source, serial u32, mime_type string)
+	destroy fn (source &C.wlr_data_source)
+
+	dnd_drop   fn (source &C.wlr_data_source)
+	dnd_finish fn (source &C.wlr_data_source)
+	dnd_action fn (source &C.wlr_data_source, dnd_action Wl_data_device_manager_dnd_action)
 }
 
 pub struct C.wlr_data_source {
@@ -52,7 +62,7 @@ pub struct C.wlr_data_source {
 
 	accepted bool
 
-	current_dnd_action C.wl_data_device_manager_dnd_action
+	current_dnd_action Wl_data_device_manager_dnd_action
 	compositor_action  u32
 
 	events struct {
@@ -62,7 +72,7 @@ pub struct C.wlr_data_source {
 
 pub struct C.wlr_drag_icon {
 	drag    &C.wlr_drag = unsafe { nil }
-	surface C.wlr_surface
+	surface &C.wlr_surface
 
 	events struct {
 		destroy C.wl_signal
@@ -86,13 +96,13 @@ pub struct C.wlr_drag {
 	pointer_grab C.wlr_seat_keyboard_grab
 	touch_grab   C.wlr_seat_touch_grab
 
-	seat         C.wlr_seat
-	seat_client  C.wlr_seat_client
-	focus_client C.wlr_seat_client
+	seat         &C.wlr_seat
+	seat_client  &C.wlr_seat_client
+	focus_client &C.wlr_seat_client
 
-	icon   C.wlr_drag_icon
-	focus  C.wlr_surface
-	source C.wlr_data_source
+	icon   &C.wlr_drag_icon
+	focus  &C.wlr_surface
+	source &C.wlr_data_source
 
 	started    bool
 	dropped    bool
@@ -119,13 +129,30 @@ pub struct C.wlr_drag {
 }
 
 pub struct C.wlr_drag_motion_event {
-	drag C.wlr_drag
+	drag &C.wlr_drag
 	time u32
 	sx   f64
 	sy   f64
 }
 
 pub struct C.wlr_drag_drop_event {
-	drag C.wlr_drag
+	drag &C.wlr_drag
 	time u32
 }
+
+pub fn C.wlr_data_device_manager_create(display &C.wl_display) &C.wlr_data_device_manager
+
+pub fn C.wlr_seat_request_set_selection(seat &C.wlr_seat, client &C.wlr_seat_client, source &C.wlr_data_source, serial u32)
+pub fn C.wlr_seat_set_selection(seat &C.wlr_seat, source &C.wlr_data_source, serial u32)
+pub fn C.wlr_drag_create(client &C.wlr_seat_client, source &C.wlr_data_source, icon_surface &C.wlr_surface, grab_type Wlr_drag_grab_type) &C.wlr_drag
+pub fn C.wlr_seat_request_start_drag(seat &C.wlr_seat, drag &C.wlr_drag, origin &C.wlr_surface, serial u32)
+pub fn C.wlr_seat_start_drag(seat &C.wlr_seat, drag &C.wlr_drag, serial u32)
+pub fn C.wlr_seat_start_pointer_drag(seat &C.wlr_seat, drag &C.wlr_drag, serial u32)
+pub fn C.wlr_seat_start_touch_drag(seat &C.wlr_seat, drag &C.wlr_drag, serial u32, point &C.wlr_touch_point)
+pub fn C.wlr_data_source_init(source &C.wlr_data_source, impl &C.wlr_data_source_impl)
+pub fn C.wlr_data_source_send(source &C.wlr_data_source, mime_type string, fd int)
+pub fn C.wlr_data_source_accept(source &C.wlr_data_source, serial u32, mime_type string)
+pub fn C.wlr_data_source_destroy(source &C.wlr_data_source)
+pub fn C.wlr_data_source_dnd_drop(source &C.wlr_data_source)
+pub fn C.wlr_data_source_dnd_finish(source &C.wlr_data_source)
+pub fn C.wlr_data_source_dnd_action(source &C.wlr_data_source, dnd_action Wl_data_device_manager_dnd_action)
