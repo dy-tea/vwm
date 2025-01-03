5063552ed63b01765ec83fa3a47e12821f89e953
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-01-03 21:39:30 +0000

    fix: impl missing allocator.h functions

--- src/wlr/allocator.v
+++ src/wlr/allocator.v
@@ -11,17 +11,22 @@ module wlr
 
 #include "wlr/render/allocator.h"
 
-pub struct C.wlr_buffer {}
-
 pub struct C.wlr_allocator_interface {
-	create_buffer C.wlr_buffer
-	destroy       voidptr
+	create_buffer fn (alloc &C.wlr_allocator, width int, height int, format &C.wlr_drm_format) &C.wlr_buffer
+	destroy       fn (alloc &C.wlr_allocator)
 }
 
+pub fn C.wlr_allocator_init(alloc &C.wlr_allocator, impl &C.wlr_allocator_interface, buffer_caps u32)
+
 pub struct C.wlr_allocator {
-	impl        C.wlr_allocator_interface
+	impl        &C.wlr_allocator_interface
 	buffer_caps u32
 	events      struct {
 		destroy C.wl_signal
 	}
 }
+
+pub fn C.wlr_allocator_autocreate(backend &C.wlr_backend, renderer &C.wlr_renderer) &C.wlr_allocator
+pub fn C.wlr_allocator_destroy(alloc &C.wlr_allocator)
+
+pub fn C.wlr_allocator_create_buffer(alloc &C.wlr_allocator, width int, height int, format &C.wlr_drm_format) &C.wlr_buffer
